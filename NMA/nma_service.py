a4aee6ce667a64166931a0a56b3c90bf15e26a0c
Adi Levi <104142857+adi-git33@users.noreply.github.com>
2025-05-26 12:55:34 +0300

    fix: fixed parameters

--- NMA/nma_service.py
+++ NMA/nma_service.py
@@ -4,63 +4,75 @@ from NMA.classes.dendrogram import Dendrogram
 from NMA.classes.edges_dataframe import EdgesDataframe
 from NMA.classes.nma import NMA
 from NMA.dataset_service import _get_dataset_config, _load_dataset
-from NMA.model_service import _get_model_path, _load_model
+from NMA.model_service import _get_model_filename, _load_model
 
-def _create_nma(model_file, graph_type, dataset_str, user, min_confidence, top_k, model_id_md):
-    if model_file is None:
-        raise ValueError("Model file cannot be None")
+# def _create_nma(model_file, graph_type, dataset_str, user, min_confidence, top_k, model_id_md):
+def _create_nma(user_id, model_id, graph_type, dataset, min_confidence, top_k):
+    if user_id is None:
+        raise ValueError("user_id cannot be None")
+    
+    if model_id is None:
+        raise ValueError("model_id cannot be None")
         
     if graph_type != GraphTypes.SIMILARITY.value and graph_type != GraphTypes.DISSIMILARITY.value and graph_type != GraphTypes.COUNT.value:
         raise ValueError("Graph type must be either 'similarity', 'dissimilarity', or 'count'")
 
+    model_path = _get_model_filename(user_id, model_id, graph_type)
+    if model_path is None:
+        raise ValueError("Model path cannot be None")
     
-    dataset_config = _get_dataset_config(dataset_str)
-    dataset = _load_dataset(dataset_config)
+    print(f"Model path: {model_path}")
+    return model_path
     
-    try:
-        loaded_model = _load_model(dataset_str, model_file.name, dataset_config)
-        model_directory = _get_model_path(user.user_id, loaded_model.model_path)
-        if model_directory is None:
-            raise ValueError("Model directory cannot be None")
+    
+    
+    # dataset_config = _get_dataset_config(dataset_str)
+    # dataset = _load_dataset(dataset_config)
+    
+    # try:
+    #     loaded_model = _load_model(dataset_str, model_file.name, dataset_config)
+    #     model_directory = _get_model_path(user.user_id, loaded_model.model_path)
+    #     if model_directory is None:
+    #         raise ValueError("Model directory cannot be None")
             
-        dataframe_filename = f'{model_directory}/{graph_type}/edges_df.csv'
-        dendrogram_filename = f'{model_directory}/{graph_type}/dendrogram'
+    #     dataframe_filename = f'{model_directory}/{graph_type}/edges_df.csv'
+    #     dendrogram_filename = f'{model_directory}/{graph_type}/dendrogram'
         
-        labels = dataset.labels
-        if dataset_str == DatasetsEnum.IMAGENET.value:
-            labels = dataset.directory_labels
+    #     labels = dataset.labels
+    #     if dataset_str == DatasetsEnum.IMAGENET.value:
+    #         labels = dataset.directory_labels
 
-        nma = NMA(
-            loaded_model.model,
-            dataset,
-            labels,
-            graph_type=graph_type,
-            top_k=top_k,
-            min_confidence=min_confidence           
-        )
+    #     nma = NMA(
+    #         loaded_model.model,
+    #         dataset,
+    #         labels,
+    #         graph_type=graph_type,
+    #         top_k=top_k,
+    #         min_confidence=min_confidence           
+    #     )
         
-        edges_df_obj = EdgesDataframe(model_file.name, dataframe_filename, nma.edges_df)
-        edges_df_obj.save_dataframe()
-        print(nma.Z)            
+    #     edges_df_obj = EdgesDataframe(model_file.name, dataframe_filename, nma.edges_df)
+    #     edges_df_obj.save_dataframe()
+    #     print(nma.Z)            
         
-        dendrogram = Dendrogram(dendrogram_filename, nma.Z)
-        dendrogram._build_tree_hierarchy(nma.Z, dataset.labels)
+    #     dendrogram = Dendrogram(dendrogram_filename, nma.Z)
+    #     dendrogram._build_tree_hierarchy(nma.Z, dataset.labels)
         
-        dendrogram.save_dendrogram(nma.Z)
+    #     dendrogram.save_dendrogram(nma.Z)
         
-        init_sub_z = dendrogram.get_sub_dendrogram_formatted(dataset_config["init_selected_labels"])
-        print(init_sub_z)
+    #     init_sub_z = dendrogram.get_sub_dendrogram_formatted(dataset_config["init_selected_labels"])
+    #     print(init_sub_z)
 
-        update_current_model(
-            user,
-            model_id_md,
-            graph_type,
-            model_file.filename,
-            dataset,
-            min_confidence,
-            top_k,
-        )
-        return init_sub_z
+    #     update_current_model(
+    #         user,
+    #         model_id_md,
+    #         graph_type,
+    #         model_file.filename,
+    #         dataset,
+    #         min_confidence,
+    #         top_k,
+    #     )
+    #     return init_sub_z
     
-    except Exception as e:
-        raise RuntimeError(f"Error during NMA creation: {str(e)}")
+    # except Exception as e:
+    #     raise RuntimeError(f"Error during NMA creation: {str(e)}")
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 from NMA.nma_service import _create_nma
 
 def main():
-    model_file = os.getenv("model_file")
-    dataset = os.getenv("dataset")
-    graph_type = os.getenv("graph_type")
+    user_id = os.getenv("user_id")
     model_id = os.getenv("model_id")
+    graph_type = os.getenv("graph_type")
+    dataset = os.getenv("dataset")
     min_confidence = float(os.getenv("min_confidence", 0.5))
-    top_k = int(os.getenv("top_k", 5))
-    result = _create_nma(model_file, graph_type, dataset, user=None, min_confidence=min_confidence, top_k=top_k, model_id_md=model_id)
+    top_k = int(os.getenv("top_k", 4))
+    result = _create_nma(user_id, model_id, graph_type, dataset, min_confidence, top_k)
     print("NMA result:", result)
 
 if __name__ == "__main__":
